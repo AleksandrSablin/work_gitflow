582c79f3153a0e470c3c23e6587d648d2169b940
AleksandrSablin <sashuta-sablin@list.ru>
2023-04-27 17:33:39 +0300

    create function 'capitalize_words'

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def str_func(words):
     return words.upper
+
+def capitalize_words(sentence):
+    '''функция делает заглавными первые буквы каждого слова в строке'''
+    words = sentence.split()
+    capitalized_words = [word.capitalize() for word in words]
+    return ' '.join(capitalized_words)
